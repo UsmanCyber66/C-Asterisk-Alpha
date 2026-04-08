2d9bf5429ab6d20dae17e5f06a0a0db20ea7db0b
Whispers91 <jackiboy567@gmail.com>
2026-04-08 21:02:37 +0200

    Testing Lexer

--- src/lexer.py
+++ src/lexer.py
@@ -1,8 +1,1 @@
-class Lexer:
-    def __init__(self, source_code):
-        self.source_code = source_code
-
-    def tokenize(self):
-        print("   -> [Lexer is working on the code...]")
-       
-        return ["dummy_token"]
+print("Hello World")
