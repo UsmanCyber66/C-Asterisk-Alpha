4470d2ed09dc3befd0032ffd3ecfc8324e180d1a
TheJudge26 <danialawny26@gmail.com>
2026-04-08 15:34:28 +0200

    Initial commit: Setup C* compiler backbone

---
+++ src/parser.py
@@ -0,0 +1,8 @@
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+    def parse(self):
+        print("   -> [Parser is building the tree...]")
+        
+        return "dummy_tree"
